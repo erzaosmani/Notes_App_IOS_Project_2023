61e058b93a8043825fa322e59fa69ea4721e596c
Erza Osmani <94173748+erzaosmani@users.noreply.github.com>
2023-09-24 15:55:57 +0200

    Added Core Data support for saving new notes in the saveAction method

--- Notes_App_IOS_Project_2023/Notes_App_IOS_Project_2023/NoteDetailVC.swift
+++ Notes_App_IOS_Project_2023/Notes_App_IOS_Project_2023/NoteDetailVC.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CoreData
 
 class NoteDetailVC: UIViewController {
 
@@ -13,7 +14,24 @@ class NoteDetailVC: UIViewController {
 
 
     @IBAction func saveAction(_ sender: Any) {
-        
+	let appDelegate = UIApplication.shared.delegate as! AppDelegate
+	let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+	let entity = NSEntityDescription.entity(forEntityName: "Note", in: context)
+	let newNote = Note(entity: entity!, insertInto: context)
+	newNote.id = noteList.count as NSNumber
+	newNote.title = titleTF.text
+	newNote.desc = descTV.text
+	do
+		{
+		try context.save()
+		noteList.append(newNote)
+		navigationController?.popViewController(animated: true)
+		}
+	    catch
+		{
+		print("context save error")
+		}
+	    
     }
 }
 	
