d03a5ef64f8e877fac52288a6d02127e57a5fc46
Erza Osmani <94173748+erzaosmani@users.noreply.github.com>
2023-09-24 15:41:37 +0200

    Added NoteTableView class for displaying a list of notes in a UITableView

--- Notes_App_IOS_Project_2023/Notes_App_IOS_Project_2023/NoteTableView.swift
+++ Notes_App_IOS_Project_2023/Notes_App_IOS_Project_2023/NoteTableView.swift
@@ -1,5 +1,23 @@
 import UIKit
-
+ var noteList = [Note]()
 class NoteTableView:UITableViewController{
+   
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+	{
+		let noteCell = tableView.dequeueReusableCell(withIdentifier: "noteCellID", for: indexPath) as! NoteCell
+
+		let thisNote: Note!
+		thisNote = noteList[indexPath.row]
+
+		noteCell.titleLabel.text = thisNote.title
+		noteCell.descLabel.text = thisNote.desc
+
+		return noteCell
+	}
+override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+	{
+		return noteList.count
+	}
     
 }
